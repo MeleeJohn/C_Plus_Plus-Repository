8343fa2f90fd2b630c6dd0561d0a319448fe8cfc
John Miele <mielejohn4@gmail.com>
2021-12-19 21:00:59 -0500

    Completed Dice Roller

--- AnotherTestingProject/ConsoleApplication1/ConsoleApplication1.cpp
+++ AnotherTestingProject/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,9 +2,51 @@
 //
 
 #include <iostream>
+#include <random>
+
+using namespace std;
+int dieType = 0;
+int dieAmount;
+int rollModifiers;
+int rollResult;
+int tempRollHolder;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+    //This is a Dice roller
+
+    cout << "How many dice? ";
+    cin >> dieAmount;
+    cout << endl;
+
+    cout << "Which type of die to roll: D";
+    cin >> dieType;
+    cout << endl;
+
+    cout << "Any modifiers? ";
+    cin >> rollModifiers;
+    cout << endl;
+
+    for (int i = 0; i < dieAmount; i++) {
+        tempRollHolder = rand() % dieType + 1;
+        rollResult += tempRollHolder;
+        cout << tempRollHolder << endl;
+    }
+
+    //rollResult = rand() % 100 + 1;
+ 
+    rollResult += rollModifiers;
+
+    if (rollModifiers != 0) {
+        cout << dieAmount << "D" << dieType << " + " << rollModifiers << " = " << rollResult << endl;
+    }
+    else {
+        cout << dieAmount << "D" << dieType << " = " << rollResult << endl;
+    }
+
+
+   
+
 }
 
