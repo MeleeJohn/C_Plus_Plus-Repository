26c6137022ebe9f7a6551966a0730099e0413368
John Miele <mielejohn4@gmail.com>
2021-12-20 19:14:27 -0500

    DnD Character Sheet Creator

---
+++ AnotherTestingProject/DnD-Class_Sheet-Creator/DnD-Class_Sheet-Creator.cpp
@@ -0,0 +1,19 @@
+// DnD-Class_Sheet-Creator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Random>
+
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << "How many dice? ";
+    //cin >> *VARIABLE*;
+    cout << endl;
+
+}
+
+
